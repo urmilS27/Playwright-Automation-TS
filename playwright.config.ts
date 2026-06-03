74806c89e5d88c89a1ce70d349a3799cbc0067e1
Urmilsakariya <urmilsakariya3727@gmail.com>
2026-06-03 15:36:40 +0530

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    // headless : false,
+    headless : true,
     screenshot : 'only-on-failure',
     video : 'retain-on-failure',
     trace: 'on-first-retry',
