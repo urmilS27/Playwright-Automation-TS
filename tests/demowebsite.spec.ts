d540f2cac2ec26088066cde98403e9061add84a0
Urmilsakariya <urmilsakariya3727@gmail.com>
2026-06-03 18:02:20 +0530

    ci check

--- tests/demowebsite.spec.ts
+++ tests/demowebsite.spec.ts
@@ -22,29 +22,31 @@ test("Date Picker Event", async ({ page }) => {
   await page.waitForTimeout(3000);
 });
 
-test("Multiple page Handler", async ({ browser }) => {
+// Failed due to bot protection
+
+// test("Multiple page Handler", async ({ browser }) => {
  
-    const context = await browser.newContext();
-    const page =await context.newPage();
+//     const context = await browser.newContext();
+//     const page =await context.newPage();
  
-    await page.goto("https://www.geeksforgeeks.org/reactjs/next-js-playwright-testing/");
+//     await page.goto("https://www.geeksforgeeks.org/reactjs/next-js-playwright-testing/");
  
-    const pagePromise = context.waitForEvent("page");
-    await page.locator(`li[value="1"] a`).click();
-    const newPage = await pagePromise;
+//     const pagePromise = context.waitForEvent("page");
+//     await page.locator(`li[value="1"] a`).click();
+//     const newPage = await pagePromise;
  
-    await newPage.waitForLoadState();
-    await newPage.locator('#ArticlePageMoreInfoNextArticleComponent_bottomCommentButton__O9Jyw').click();
+//     await newPage.waitForLoadState();
+//     await newPage.locator('#ArticlePageMoreInfoNextArticleComponent_bottomCommentButton__O9Jyw').click();
  
-    const feedBackField = newPage.frameLocator('iframe#gfg_post_1107997_ifr').locator(`html`).locator(".mce-content-body ");
-    await feedBackField.fill("Well explaination");
-    await newPage.locator('.submit_btn').click();
-    await newPage.close();
+//     const feedBackField = newPage.frameLocator('iframe#gfg_post_1107997_ifr').locator(`html`).locator(".mce-content-body ");
+//     await feedBackField.fill("Well explaination");
+//     await newPage.locator('.submit_btn').click();
+//     await newPage.close();
  
-    await page.locator('#ArticlePageMoreInfoNextArticleComponent_bottomCommentButton__O9Jyw').click();
-    const feedBackField_2 = page.frameLocator('iframe#gfg_post_1284674_ifr').locator(`html`).locator(".mce-content-body ");
-    await feedBackField_2.fill("Well explaination");
-    await page.locator('.submit_btn').click();
+//     await page.locator('#ArticlePageMoreInfoNextArticleComponent_bottomCommentButton__O9Jyw').click();
+//     const feedBackField_2 = page.frameLocator('iframe#gfg_post_1284674_ifr').locator(`html`).locator(".mce-content-body ");
+//     await feedBackField_2.fill("Well explaination");
+//     await page.locator('.submit_btn').click();
  
-    await page.waitForTimeout(2000);
-});
+//     await page.waitForTimeout(2000);
+// });
