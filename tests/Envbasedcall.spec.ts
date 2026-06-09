5c133d99dc264ae444fa8b23b2c62defc4e44693
Urmilsakariya <urmilsakariya3727@gmail.com>
2026-06-09 12:40:35 +0530

    env

--- tests/Envbasedcall.spec.ts
+++ tests/Envbasedcall.spec.ts
@@ -2,5 +2,5 @@ import { test } from '@playwright/test';
 import { ENV } from '../config/env' ;
 
 test('Call based on Environment', async ({ page }) => {
-    await page.goto(ENV.baseUrl!)
+    await page.goto(ENV.baseUrl!, {timeout : 100000})
 })
